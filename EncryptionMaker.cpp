2de1b7f8edc2e3ba33af12f81a7eb9d77054245c
mjkimdelta@gmail.com <mjkimdelta@gmail.com>
2021-04-05 14:11:56 +0900

    Make that board is made

--- EncryptionMaker.cpp
+++ EncryptionMaker.cpp
@@ -3,6 +3,7 @@
 #include <cctype>
 #include <string>
 #include <string.h>
+#include<vector>
 using namespace std;
 #define ARRAY 64
 #define KBSIZE 5
@@ -48,7 +49,7 @@ void encryption() { //암호화 수행 함수
 	std::cin.ignore(); 
 	std::cin.getline(key, ARRAY); //암호키 입력
 	int len = std::strlen(key);
-	raw_key = new char[len + 1];
+	raw_key = new char[len+1];
 	strcpy_s(raw_key, len + 1, key);
 
 	int i, j, keyidx;
@@ -59,39 +60,56 @@ void encryption() { //암호화 수행 함수
 
 	removeSpaceAndDupplication(key); //공백 제거
 	len = std::strlen(key);
-
-
-	keyidx = 0;
-	int alpbt = 65; //A
-	bool qzexist = false;
-	for (i = 0; i < KBSIZE; i++) {
-		for (j = 0; j < KBSIZE; j++) {
-			if (keyidx < len) { //key 대입
-				if (key[keyidx] == 81 || key[keyidx] == 90) {
-					if (qzexist == false) keyboard[i][j] = key[keyidx];
-					else {
-						qzexist = true;
-					} keyidx++;
-				}
-				else {
-					keyboard[i][j] = key[keyidx++];
-				}
+	
+	
+	i = 0; j = 0; bool qzexist = false; //Q or Z
+	
+	for (keyidx = 0; keyidx < len; keyidx++) //key값 보드에 배치
+	{
+		if (key[keyidx] == 81 || key[keyidx] == 90) { //if Q or Z
+			if (qzexist == false) {
+				keyboard[i][j] = key[keyidx];
+				qzexist = true;
+				
+				if (j >= KBSIZE - 1) {
+					i++; j = 0;
+				}else j++;
+			};
+		}
+		else {
+			keyboard[i][j] = key[keyidx];
+			if (j >= KBSIZE - 1) {
+				i++; j = 0;
 			}
-			else { //남은 알파벳 대입
-				//if Q and Z
-				if ((alpbt == 81 || alpbt == 90)) {
-					if (qzexist == false) keyboard[i][j] = (char)alpbt;
-					alpbt++;
-				}
-				else {
-					//이미 해당알파벳이 key에 존재하는(사용된) 경우
-					if (strchr(key, (char)alpbt) == NULL) keyboard[i][j] = (char)alpbt;
-					alpbt++;
-				}
-			}
+			else j++;
 		}
 	}
-
+	
+	int alpbt = 65; //A
+	while (i<KBSIZE && j<KBSIZE) { //보드 나머지 채우기
+		//if Q and Z
+		if (alpbt == 81 || alpbt == 90) {
+			if (qzexist == false) {//Q나 Z가 아직 없으면
+				keyboard[i][j] = alpbt;
+				qzexist = true;
+				if (j >= KBSIZE - 1) {
+					i++; j = 0;
+				}
+				else j++;
+			}
+		}
+		else { //Q와 Z를 제외한 모든 알파벳
+			if (!strchr(key, alpbt)) {//해당알파벳이 key에 있지 않은 경우
+				keyboard[i][j] = alpbt;
+				if (j >= KBSIZE - 1) {
+					i++; j = 0;
+				}
+				else j++;
+			}
+		}
+		alpbt++;
+	}
+	
 	for (i = 0; i < KBSIZE; i++) {
 		for (j = 0; j < KBSIZE; j++) {
 			std::cout << "_"<<keyboard[i][j]<< " ";
