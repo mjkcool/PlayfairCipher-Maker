6c5987b6c48c641700b80b2ef0c16da79501b995
mjkimdelta@gmail.com <mjkimdelta@gmail.com>
2021-04-05 15:54:06 +0900

    Initialize file handling func and separate secondary funcs that process space and playfair

--- EncryptionMaker.cpp
+++ EncryptionMaker.cpp
@@ -11,41 +11,49 @@ using namespace std;
 //─ │ ┌ ┐ ┘ └ ├ ┬ ┤ ┴ ┼ ━ ┃ ┏ ┓ ┛ ┗ ┣ ┳ ┫ ┻ ╋ ┠ ┯ ┨ ┷ ┿ ┝ ┰ ┥ ┸ ╂ ┒ ┑ ┚ ┙ ┖ ┕ ┎ ┍ ┞ ┟ ┡ ┢ ┦ ┧ ┩ ┪ ┭ ┮ ┱ ┲ ┵ ┶ ┹ ┺ ┽ ┾ ╀ ╁ ╃ ╄ ╅ ╆ ╇ ╈ ╉ ╊
 //Alphabet ASCII 65~90
 
-void removeSpaceAndDupplication(char* key) { //입력받은 문장의 공백을 제거
+void removeSpace(char* str) { //입력받은 문장의 공백을 제거
 	int i, index = 0;
 
-	for (i = 0; key[i] != 0; i++) {
-		if (key[i] != ' ') {
-			key[index] = key[i];
+	for (i = 0; str[i] != 0; i++) {
+		if (str[i] != ' ') {
+			str[index] = str[i];
 			index++;
 		}
 	}
-	key[index] = 0; //배열 종료
-	char newkey[ARRAY]; //중복제거된 문자열
-	newkey[0] = key[0];
-	int newkeyi = 0;
-	for (i = 1; key[i] != 0; i++) { //배열끝까지 반복
-		bool exi = false;
-		for (int j = 0; j < i; j++) if (key[i] == key[j]) exi = true; //앞서 있는 값이면 체크
-		if(exi==false) newkey[++newkeyi] = key[i]; //앞서 없는 값이면 추가.
-	}
-	newkey[++newkeyi] = 0; //배열 종료
+	str[index] = 0; //배열 종료
 	
-	for (i=0; newkey[i] != 0; i++) { 
-		key[i] = newkey[i]; //복사~!
-	}
-	key[i] = 0;
 }
 
+void removeDupplication(char* str) {
+	int i;
+	char newkey[ARRAY]; //중복제거된 문자열
+	newkey[0] = str[0];
+	int newkeyi = 0;
+	for (i = 1; str[i] != 0; i++) { //배열끝까지 반복
+		bool exi = false;
+		for (int j = 0; j < i; j++) if (str[i] == str[j]) exi = true; //앞서 있는 값이면 체크
+		if (exi == false) newkey[++newkeyi] = str[i]; //앞서 없는 값이면 추가.
+	}
+	newkey[++newkeyi] = 0; //배열 종료
 
+	for (i = 0; newkey[i] != 0; i++) {
+		str[i] = newkey[i]; //복사~!
+	}
+	str[i] = 0;
+}
+
+void separateToPlayfair(char* str) {
+
+}
 
 void encryption() { //암호화 수행 함수
 	char key[ARRAY]; //사용될 암호키
 	char* raw_key; //암호키 원문
+	char plaintxt[ARRAY];
 	char keyboard[KBSIZE][KBSIZE]; //암호판
 
-	std::cout << "\n┏━━━━ Processing... ━━━━ Encryption ━━━━┓\n┃\n";
-	std::cout << "┣━■ Enter your Cipher key\n(*Only in Eng, till " << ARRAY - 1 << " letter include spaces)\n : ";
+	std::cout << "\n━━━━ Processing... ━━━━ Encryption ━━━━━\n\n";
+	std::cout << "Enter your Cipher key\n(*Only in Eng, till " << ARRAY - 1 << " letter include spaces)\n : ";
 	std::cin.ignore(); 
 	std::cin.getline(key, ARRAY); //암호키 입력
 	int len = std::strlen(key);
@@ -58,7 +66,8 @@ void encryption() { //암호화 수행 함수
 		key[keyidx] = (char)toupper(key[keyidx]);
 	}
 
-	removeSpaceAndDupplication(key); //공백 제거
+	removeSpace(key); //공백 제거
+	removeDupplication(key);
 	len = std::strlen(key);
 	
 	
@@ -109,7 +118,10 @@ void encryption() { //암호화 수행 함수
 		}
 		alpbt++;
 	}
-	
+
+	//암호판 출력
+	//std::cout << "\nInput key
+	std::cout << "\n_Cipher board *position of Q and Z is same as one.\n\n";
 	for (i = 0; i < KBSIZE; i++) {
 		for (j = 0; j < KBSIZE; j++) {
 			std::cout << "_"<<keyboard[i][j]<< " ";
@@ -117,8 +129,23 @@ void encryption() { //암호화 수행 함수
 		std::cout << "\n";
 	}
 
-	std::cin.clear();
 	
+	std::cin.ignore();
+	std::cout << "Enter your plain text\n(*Only in Eng, till " << ARRAY - 1 << " letter include spaces)\n : \n";
+	std::cin.getline(plaintxt, ARRAY);
+	//암호화
+	int txtidx;
+	for (txtidx = 0; txtidx < len; txtidx++) { //대문자로 변환
+		plaintxt[txtidx] = (char)toupper(plaintxt[txtidx]);
+	}
+	removeSpace(plaintxt);//공백제거
+	separateToPlayfair(plaintxt);
+	
+
+	
+
+
+	std::cin.clear();
 	return;
 }
 
@@ -127,26 +154,44 @@ void decryption() { //복호화 수행 함수
 	return;
 }
 
+void fileEncryption() {
+
+}
+
+void fileDecryption() {
+
+}
+
 int main()
 {
 	std::cout << "Playfair Substitution Cipher Maker\n\n";
-	std::cout << "■[enter 'E'] Encryption ■\n";
-	std::cout << "■[enter 'D'] Decryption ■\n";
+	std::cout << "■[enter 1] Input encryption ■\n";
+	std::cout << "■[enter 2] Input decryption ■\n";
+	std::cout << "■[enter 3] .txt file Encryption ■\n";
+	std::cout << "■[enter 4] .txt file Decryption ■\n";
 
 	std::cout << "\nEnter character which you have to do >> ";
 	while (true) {
 		char process;
 		std::cin >> process;
-		if (process == 'E') {
+		if (process == '1') {
 			encryption();
 			break;
 		}
-		else if (process == 'D') {
+		else if (process == '2') {
 			decryption();
 			break;
 		}
+		else if (process == '4') {
+			fileEncryption();
+			break;
+		}
+		else if (process == '5') {
+			fileDecryption();
+			break;
+		}
 		else {
-			std::cout << "a null character. Enter again >> ";
+			std::cout << "this is invalid number to process order. Enter again >> ";
 		}
 	}
 
